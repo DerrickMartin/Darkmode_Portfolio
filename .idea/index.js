bfe3684730dc3d674bea39eaa5bac115b2910635
Derrick Hannah <30300627+DerrickMartin@users.noreply.github.com>
2021-12-14 16:32:52 -0800

    updated css adding root theme, added css variables and prefers-color-scheme

--- .idea/index.js
+++ .idea/index.js
@@ -3,3 +3,10 @@ const switchElement = document.querySelector('.switch')
 switchElement.addEventListener('click', () => {
     document.body.classList.toggle('dark')
 })
+
+let darkmode = localStorage.getItem('darkmode');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+darkModeToggle.addEventListener('click', () => {
+    console.log("Here is Dark Mode");
+})
